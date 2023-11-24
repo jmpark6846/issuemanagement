3f1af6b0fb9065ba0213451a8a14f157dd343e6e
jmpark6846 <jmpark6846@gmail.com>
2023-11-24 21:40:54 +0900

    add database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+const NODE_ENV = process.env.NODE_ENV;
+
+if (NODE_ENV !== 'dev' && NODE_ENV !== 'prod') {
+  throw new Error('NODE_ENV not set');
+}
 
 @Module({
   imports: [
@@ -12,6 +18,18 @@ import { UserModule } from './user/user.module';
       cache: true,
       envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: NODE_ENV === 'dev', // always 'false' on production
+    }),
+
     AuthModule,
     UserModule,
   ],
