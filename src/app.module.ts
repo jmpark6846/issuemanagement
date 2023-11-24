dbec7853023c0026829b8b376e7b52215d9e00f2
jmpark6846 <jmpark6846@gmail.com>
2023-11-24 16:01:51 +0900

    add user, auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`cwd: ${process.cwd()} `);
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +12,8 @@ console.log(`cwd: ${process.cwd()} `);
       cache: true,
       envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
     }),
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
